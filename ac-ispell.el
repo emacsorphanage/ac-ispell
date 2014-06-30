b88be5a61ab6b0059fb2f19f995b5c3a76049bc0
igjit <igjit1@gmail.com>
2014-06-30 20:36:34 +0900

    Implement correction source

--- ac-ispell.el
+++ ac-ispell.el
@@ -55,6 +55,16 @@
   :type 'integer
   :group 'ac-ispell)
 
+(defcustom ac-ispell-correction-limit 6
+  "Limit number of candidates for correction source."
+  :type 'integer
+  :group 'ac-ispell)
+
+(defface ac-ispell-correction-candidate-face
+  '((t (:inherit ac-candidate-face :foreground "red")))
+  "Face for correction candidate."
+  :group 'ac-ispell)
+
 (defvar ac-ispell--cache nil)
 
 (defun ac-ispell--case-function (input)
@@ -86,10 +96,29 @@
                             (ac-ispell--lookup-candidates lookup-func input))))
         (mapcar case-func candidates)))))
 
+(defun ac-ispell--correct-word (word)
+  (when (and ispell-async-processp
+             (< 0 (length word)))
+    (ispell-set-spellchecker-params)
+    (ispell-accept-buffer-local-defs)
+    (ispell-send-string "%\n")
+    (ispell-send-string (concat "^" word "\n"))
+    (while (progn
+             (with-local-quit
+               (accept-process-output ispell-process nil nil 1))
+             (not (string= "" (car ispell-filter)))))
+    (let ((poss (ispell-parse-output (cadr ispell-filter))))
+      (if (listp poss)
+          (nth 2 poss)))))
+
+(defun ac-ispell--correction-candidates ()
+  (ac-ispell--correct-word ac-prefix))
+
 ;;;###autoload
 (defun ac-ispell-ac-setup ()
   "Add `ac-source-ispell' to `ac-sources' and enable `auto-complete' mode"
   (interactive)
+  (add-to-list 'ac-sources 'ac-source-ispell-correction)
   (add-to-list 'ac-sources 'ac-source-ispell)
   (unless auto-complete-mode
     (auto-complete-mode +1)))
@@ -102,7 +131,14 @@
   (ac-define-source ispell
     `((candidates . ac-ispell--candidates)
       (requires . ,ac-ispell-requires)
-      (symbol . "s"))))
+      (symbol . "s")))
+  (ac-define-source ispell-correction
+    `((candidates . ac-ispell--correction-candidates)
+      (match . (lambda (prefix candidates) candidates))
+      (requires . ,ac-ispell-requires)
+      (limit . ,ac-ispell-correction-limit)
+      (symbol . "s")
+      (candidate-face . ac-ispell-correction-candidate-face))))
 
 (provide 'ac-ispell)
 
